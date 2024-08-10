bd55d47aeb9c4a0a455be5380626fe5af2df4f0d
MakotoTaguchi <taguchimakoto@icloud.com>
2024-08-10 18:55:22 +0900

    ListViewの画像差し替え

--- ChatViewWithSwiftUi/Views/ListView.swift
+++ ChatViewWithSwiftUi/Views/ListView.swift
@@ -11,48 +11,62 @@ struct ListView: View {
     var body: some View {
         VStack {
             // Header
-            HStack {
-                Text("トーク")
-                    .font(.title2.bold())
-                
-                Spacer()
-                
-                HStack {
-                    Circle()
-                        .frame(width: 40, height: 40)
-                    Circle()
-                        .frame(width: 40, height: 40)
-                    Circle()
-                        .frame(width: 40, height: 40)
-                }
-            }
-            
+            header
+
             // List
-            ScrollView {
-                VStack {
-                    ForEach(0..<5) {_ in
-                        HStack {
-                            Circle()
-                                .frame(width: 60, height: 60)
-                            VStack(alignment: .leading) {
-                                Text("タイトル")
-                                Text("最新のメッセージ")
-                                    .font(.footnote)
-                                    .foregroundColor(Color(uiColor: .secondaryLabel))
-                            }
-                            Spacer()
-                            Text("12/31")
-                                .font(.caption)
-                                .foregroundColor(Color(uiColor: .secondaryLabel))
-                        }
-                    }
-                }
-            }
+            list
         }
-        .padding(.horizontal)
+            .padding(.horizontal)
     }
 }
 
 #Preview {
     ListView()
+}
+
+extension ListView {
+    private var header: some View {
+        HStack {
+            Text("トーク")
+                .font(.title2.bold())
+
+            Spacer()
+
+            HStack(spacing: 16) {
+                Image(systemName: "text.badge.checkmark")
+                Image(systemName: "square")
+                Image(systemName: "ellipsis.bubble")
+            }
+            .font(.title2)
+        }
+    }
+
+    private var list: some View {
+        ScrollView {
+            VStack {
+                ForEach(0..<5) { _ in
+                    listRow
+                }
+            }
+        }
+    }
+
+    private var listRow: some View {
+        HStack {
+            Image("user01")
+                .resizable()
+                .frame(width: 48, height: 48)
+                .clipShape(Circle())
+            VStack(alignment: .leading) {
+                Text("タイトル")
+                Text("最新のメッセージ")
+                    .font(.footnote)
+                    .foregroundColor(Color(uiColor: .secondaryLabel))
+            }
+            Spacer()
+            Text("12/31")
+                .font(.caption)
+                .foregroundColor(Color(uiColor: .secondaryLabel))
+        }
+    }
 }
